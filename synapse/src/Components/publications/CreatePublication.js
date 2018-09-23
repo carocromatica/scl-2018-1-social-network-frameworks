96420e0ef225eb7e1608e50d95b35d0631d6afda
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-09-23 01:25:11 -0300

    archivos de post

---
+++ synapse/src/Components/publications/CreatePublication.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { createPublication } from '../../store/actions/publicationActions'
+import { Redirect } from 'react-router-dom'
+
+class createPublication extends Component {
+  state = {
+    title: '',
+    content: ''
+  }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(this.state);
+    this.props.createPublication(this.state);
+  
+  }
+  render() {
+    const { auth } = this.props;
+    if (!auth.uid) return <Redirect to='/signin' /> 
+    return (
+      <div className="container">
+        <form className="white" onSubmit={this.handleSubmit}>
+          <h5 className="grey-text text-darken-3">Create Post</h5>
+          <div className="input-field">
+            <input type="text" id='title' onChange={this.handleChange} />
+            <label htmlFor="title">Title</label>
+          </div>
+          <div className="input-field">
+            <textarea id="content" className="materialize-textarea" onChange={this.handleChange}></textarea>
+            <label htmlFor="content">Content</label>
+          </div>
+          <div className="input-field">
+            <button className="btn pink lighten-1">Create</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    createProject: (publication) => dispatch(createProject(publication))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(createPublication)
