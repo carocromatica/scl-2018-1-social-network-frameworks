155e07130caa8b9f19b65acc4cbb8c809e919443
antonella <antonella_caroca@hotmail.com>
2018-09-24 02:57:51 -0300

    agrego eventos

--- synapse/src/Components/pages/Events.js
+++ synapse/src/Components/pages/Events.js
@@ -24,10 +24,58 @@ class Events extends Component {
         <IconBar /> 
         </div>
         SOY EVENTOS
+        <div className='divEventos'>
+                <i class="fas fa-user fa-2x"></i>
+                <p>CREADORA: Antonella Caroca</p>
+                <h5>NOMBRE EVENTO:</h5>
+                <h4>Coding in sign language!</h4>
+                <h5>¿CUÁNDO?</h5>
+                <h4>5/Noviembre/2018</h4>
+                <h5>DESCRIPCIÓN DEL EVENTO:</h5>
+                <p>Programación accesible en lengua de señas para la comunidad sorda! Vengan a este super evento que incluye a destacados hackers internacionales y la comunidad sorda!</p>
+                <i class="far fa-calendar-check"></i>
+            </div>
+             <hr/>
           </div>
-          
+          <div className='divEventos'>
+                <i class="fas fa-user fa-2x"></i>
+                <p>CREADORA: Caro Torres</p>
+                <h5>NOMBRE EVENTO:</h5>
+                <h4>Programación y Diseño!</h4>
+                <h5>¿CUÁNDO?</h5>
+                <h4>7/Noviembre/2018</h4>
+                <h5>DESCRIPCIÓN DEL EVENTO:</h5>
+                <p>Programación y Diseño es un evento que mezcla lo mejor de estos dos mundos. Vienen destacadas diseñadoras y programadoras de latinoamerica.</p>
+                <i class="far fa-calendar-check"></i>
+            </div>
+             <hr/>
+             <div className='divEventos'>
+                <i class="fas fa-user fa-2x"></i>
+                <p>CREADORA: Pamela Rojas</p>
+                <h5>NOMBRE EVENTO:</h5>
+                <h4>Inteligencia Artificial</h4>
+                <h5>¿CUÁNDO?</h5>
+                <h4>10/Noviembre/2018</h4>
+                <h5>DESCRIPCIÓN DEL EVENTO:</h5>
+                <p>¿Qué es la inteligencia artificial? Este es un evento para todas aquellas personas que deseen aventurarse en el mundo de la inteligencia artificial.
+                Se repasarán conceptos básicos y no se requiere conocimiento previo para participar. Todas invitadas!</p>
+                <i class="far fa-calendar-check"></i>
+            </div>
+             <hr/>
+             <div className='divEventos'>
+                <i class="fas fa-user fa-2x"></i>
+                <p>CREADORA: Francisca Zúñiga</p>
+                <h5>NOMBRE EVENTO:</h5>
+                <h4>Desarrollo y perfeccionamiento de habilidades blandas</h4>
+                <h5>¿CUÁNDO?</h5>
+                <h4>12/Noviembre/2018</h4>
+                <h5>DESCRIPCIÓN DEL EVENTO:</h5>
+                <p>¿Cómo logras formar o integrarte a un equipo de trabajo sin morir en el intento? Aprende a potenciar tus habilidades blandas y a transferirlas a tu trabajo </p>
+                <i class="far fa-calendar-check"></i>
+            </div>
+             <hr/>
+          </div>
         </div>
-      </div>
       </div>
     )
   }
